0c53c2a92b5997a588fa1cd384d60b7a860b70e3
h f <waltermelonson@gmail.com>
2019-03-27 17:49:43 -0700

    initial creation

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCzula4zKqJSnSxmbvOVA5bpuOJ_6D1eeE",
+    authDomain: "trainstation-b5018.firebaseapp.com",
+    databaseURL: "https://trainstation-b5018.firebaseio.com",
+    projectId: "trainstation-b5018",
+    storageBucket: "trainstation-b5018.appspot.com",
+    messagingSenderId: "234193224619"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
