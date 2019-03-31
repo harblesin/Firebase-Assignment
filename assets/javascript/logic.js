b8f5a1e84125b024c150759b1e5be582e3616874
h f <waltermelonson@gmail.com>
2019-03-31 13:32:47 -0700

    small changes from desktop

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,13 +1,66 @@
+$(document).ready(function () {
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCzula4zKqJSnSxmbvOVA5bpuOJ_6D1eeE",
-    authDomain: "trainstation-b5018.firebaseapp.com",
-    databaseURL: "https://trainstation-b5018.firebaseio.com",
-    projectId: "trainstation-b5018",
-    storageBucket: "trainstation-b5018.appspot.com",
-    messagingSenderId: "234193224619"
-  };
-  firebase.initializeApp(config);
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCzula4zKqJSnSxmbvOVA5bpuOJ_6D1eeE",
+        authDomain: "trainstation-b5018.firebaseapp.com",
+        databaseURL: "https://trainstation-b5018.firebaseio.com",
+        projectId: "trainstation-b5018",
+        storageBucket: "trainstation-b5018.appspot.com",
+        messagingSenderId: "234193224619"
+    };
+    firebase.initializeApp(config);
 
-var database = firebase.database();
+    var database = firebase.database();
+
+    var randomDate = "03/02/1997";
+    var randomFormat = "DD/MM/YYYY";
+    var convertedDate = moment(randomDate, randomFormat);
+
+    console.log(convertedDate.format("MM/DD/YY"));
+
+
+    //var newTrain = $("#train").val().trim();
+
+    $(".addBtn").click(function (event) {
+        event.preventDefault();
+        var newTrain = $("#train").val().trim();
+        var newDest = $("#dest").val().trim();
+        var newFreq = $("#freq").val().trim();
+        var firstTrain = $("#firstTrain").val().trim();
+
+
+        if (newTrain == "" || newDest == "" || newFreq == "" ||
+            firstTrain == "") {
+
+                alert("Please complete all fields before adding.")
+
+        } else {
+
+            //var minAway = $("#minAway").val().trim();
+
+            database.ref().push({
+                Train_Name: newTrain,
+                Destination: newDest,
+                Frequency: newFreq,
+                Next_Arrival: newArrival,
+                Minutes_Away: minAway
+            })
+
+            $("input").val("");
+
+        }
+    });
+
+    database.ref().on("child_added", function (childSnapshot) {
+
+        $("#trainDis").prepend($("<tr>")).prepend(childSnapshot.val().Train_Name);
+        $("#destDis").prepend($("<tr>")).prepend(childSnapshot.val().Destination);
+        $("#freqDis").prepend($("<tr>")).prepend(childSnapshot.val().Frequency);
+        $("#nextDis").prepend($("<tr>")).prepend(childSnapshot.val().Next_Arrival);
+        $("#minDis").prepend($("<tr>")).prepend(childSnapshot.val().Minutes_Away);
+
+
+    })
+
+})
